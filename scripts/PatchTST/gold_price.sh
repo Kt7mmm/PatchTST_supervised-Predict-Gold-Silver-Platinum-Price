6c593fab3ae91bcf659025a1d1b123e6e6089caa
KT <21522605@gm.uit.edu.vn>
2024-05-28 20:08:12 +0700

    fix bug

--- scripts/PatchTST/gold_price.sh
+++ scripts/PatchTST/gold_price.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-if [ ! -d "./logs" ]; then
-    mkdir ./logs
-fi
+# Kiểm tra và tạo thư mục logs nếu chưa tồn tại
+mkdir -p ./logs/LongForecasting
 
-if [ ! -d "./logs/LongForecasting" ]; then
-    mkdir ./logs/LongForecasting
-fi
-
+# Thiết lập các biến cần thiết
 seq_len=104
 model_name=PatchTST
-
 root_path_name=./dataset/
 data_path_name=gold_price.csv
 model_id_name=gold_price
 data_name=custom
-
 random_seed=2021
 
+# Vòng lặp cho các giá trị pred_len khác nhau
 for pred_len in 24 36 48 60
 do
     python -u run_longExp.py \
@@ -25,7 +20,7 @@ do
       --is_training 1 \
       --root_path $root_path_name \
       --data_path $data_path_name \
-      --model_id $model_id_name_$seq_len'_'$pred_len \
+      --model_id ${model_id_name}_${seq_len}_${pred_len} \
       --model $model_name \
       --data $data_name \
       --features M \
@@ -42,9 +37,8 @@ do
       --patch_len 24 \
       --stride 2 \
       --des 'Exp' \
-      --train_epochs 5 \  # Chạy thử với 5 epoch
-      --lradj 'constant' \
-      --itr 1 \
+      --train_epochs 5 \
       --batch_size 16 \
-      --learning_rate 0.0025 > logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
+      --learning_rate 0.0025 \
+      --itr 1 > logs/LongForecasting/${model_name}_${model_id_name}_${seq_len}_${pred_len}.log
 done
